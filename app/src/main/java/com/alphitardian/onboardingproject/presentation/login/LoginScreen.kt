a1ea75a2c8c717dfe879ab347a0b0bc1f76edd8e
Ardian Alphita <ardianpramudya81@gmail.com>
2021-10-01 13:25:00 +0700

    Refactor component from Login Screen

--- app/src/main/java/com/alphitardian/onboardingproject/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/alphitardian/onboardingproject/presentation/login/LoginScreen.kt
@@ -2,23 +2,21 @@ package com.alphitardian.onboardingproject.presentation.login
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.alphitardian.onboardingproject.R
+import com.alphitardian.onboardingproject.presentation.login.components.TextInputField
 
 @Composable
 fun LoginScreen() {
@@ -70,40 +68,6 @@ fun LoginScreen() {
                 )
             }
         }
-    }
-}
-
-@Composable
-fun TextInputField(
-    title: String,
-    isPassword: Boolean,
-    value: String,
-    onValueChange: (String) -> Unit
-) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Text(text = title, color = Color.Gray)
-        TextField(
-            value = value,
-            onValueChange = onValueChange,
-            singleLine = true,
-            placeholder = {
-                Text(text = "Type your ${title.lowercase()} here")
-            },
-            colors = TextFieldDefaults.textFieldColors(backgroundColor = Color.White),
-            visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = if (isPassword) KeyboardType.Password else KeyboardType.Email
-            ),
-            trailingIcon = {
-                if (isPassword) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_baseline_visibility_off_24),
-                        contentDescription = "Show password Icon"
-                    )
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
-        )
     }
 }
 
