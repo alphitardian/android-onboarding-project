83e3a6806f90f8754a92af73c9706a7b9700f84e
Ardian Alphita <ardianpramudya81@gmail.com>
2021-10-08 16:14:05 +0700

    Add get token usecase

--- app/src/main/java/com/alphitardian/onboardingproject/domain/use_case/get_token/GetTokenUseCase.kt
+++ app/src/main/java/com/alphitardian/onboardingproject/domain/use_case/get_token/GetTokenUseCase.kt
@@ -1,19 +1,19 @@
-package com.alphitardian.onboardingproject.domain.use_case.user_login
+package com.alphitardian.onboardingproject.domain.use_case.get_token
 
 import com.alphitardian.onboardingproject.common.Resource
-import com.alphitardian.onboardingproject.data.remote.entity.auth.LoginRequest
 import com.alphitardian.onboardingproject.data.remote.entity.auth.TokenResponse
 import com.alphitardian.onboardingproject.data.repository.auth.AuthRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
+import javax.inject.Inject
 
-class UserLoginUseCase(private val repository: AuthRepository) {
-    operator fun invoke(requestBody : LoginRequest): Flow<Resource<TokenResponse>> = flow {
+class GetTokenUseCase @Inject constructor(private val repository: AuthRepository) {
+    operator fun invoke(userToken : String): Flow<Resource<TokenResponse>> = flow {
         try {
             emit(Resource.Loading())
-            val response = repository.loginUser(requestBody)
+            val response = repository.getUserToken(userToken)
             emit(Resource.Success(data = response))
         } catch (e: HttpException) {
             emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
