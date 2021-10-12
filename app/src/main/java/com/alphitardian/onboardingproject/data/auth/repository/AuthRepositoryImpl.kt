6f94354033d5f7e1e91b8f50b1455679607c7b9e
Ardian Alphita <ardianpramudya81@gmail.com>
2021-10-12 19:10:03 +0700

    Update AuthRepository to adapt with the remote data source

--- app/src/main/java/com/alphitardian/onboardingproject/data/auth/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/alphitardian/onboardingproject/data/auth/repository/AuthRepositoryImpl.kt
@@ -1,17 +1,18 @@
 package com.alphitardian.onboardingproject.data.auth.repository
 
-import com.alphitardian.onboardingproject.data.auth.data_source.remote.network.AuthApi
+import androidx.lifecycle.LiveData
+import com.alphitardian.onboardingproject.common.Resource
+import com.alphitardian.onboardingproject.data.auth.data_source.remote.RemoteDataSource
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.LoginRequest
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.TokenResponse
 import com.alphitardian.onboardingproject.domain.repository.AuthRepository
-import retrofit2.Response
 
-class AuthRepositoryImpl(private val api: AuthApi) : AuthRepository {
-    override suspend fun loginUser(requestBody: LoginRequest): Response<TokenResponse> {
-        return api.loginUser(requestBody)
+class AuthRepositoryImpl(private val remoteDataSource: RemoteDataSource) : AuthRepository {
+    override suspend fun loginUser(requestBody: LoginRequest): LiveData<Resource<TokenResponse>> {
+        return remoteDataSource.loginUser(requestBody)
     }
 
-    override suspend fun getUserToken(userToken: String): Response<TokenResponse> {
-        return api.getUserToken(userToken)
+    override suspend fun getUserToken(userToken: String): LiveData<Resource<TokenResponse>> {
+        return remoteDataSource.getToken(userToken)
     }
 }
--- app/src/main/java/com/alphitardian/onboardingproject/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/alphitardian/onboardingproject/domain/repository/AuthRepository.kt
@@ -1,10 +1,12 @@
 package com.alphitardian.onboardingproject.domain.repository
 
+import androidx.lifecycle.LiveData
+import com.alphitardian.onboardingproject.common.Resource
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.LoginRequest
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.TokenResponse
 import retrofit2.Response
 
 interface AuthRepository {
-    suspend fun loginUser(requestBody: LoginRequest): Response<TokenResponse>
-    suspend fun getUserToken(userToken: String): Response<TokenResponse>
+    suspend fun loginUser(requestBody: LoginRequest): LiveData<Resource<TokenResponse>>
+    suspend fun getUserToken(userToken: String): LiveData<Resource<TokenResponse>>
 }
