a4f844eba7076b7ac9974d36470e9c5c668a5023
Ardian Alphita <ardianpramudya81@gmail.com>
2021-10-12 19:07:13 +0700

    Add remote data source for auth

---
+++ app/src/main/java/com/alphitardian/onboardingproject/data/auth/data_source/remote/RemoteDataSource.kt
@@ -0,0 +1,56 @@
+package com.alphitardian.onboardingproject.data.auth.data_source.remote
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.alphitardian.onboardingproject.common.ErrorState
+import com.alphitardian.onboardingproject.common.Resource
+import com.alphitardian.onboardingproject.data.auth.data_source.remote.network.AuthApi
+import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.LoginRequest
+import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.TokenResponse
+import com.alphitardian.onboardingproject.data.user.data_source.remote.network.UserApi
+
+class RemoteDataSource(private val authApi: AuthApi, private val userApi: UserApi) {
+    suspend fun loginUser(requestBody: LoginRequest): LiveData<Resource<TokenResponse>> {
+        val result = MutableLiveData<Resource<TokenResponse>>()
+        val response = authApi.loginUser(requestBody)
+
+        when {
+            response.isSuccessful -> {
+                result.postValue(Resource.Success<TokenResponse>(data = response.body()!!))
+            }
+            response.code() == 400 -> {
+                result.postValue(Resource.Error<TokenResponse>(state = ErrorState.ERROR_400))
+            }
+            response.code() == 401 -> {
+                result.postValue(Resource.Error<TokenResponse>(state = ErrorState.ERROR_401))
+            }
+            response.code() == 422 -> {
+                result.postValue(Resource.Error<TokenResponse>(state = ErrorState.ERROR_422))
+            }
+        }
+
+        return result
+    }
+
+    suspend fun getToken(userToken: String) : LiveData<Resource<TokenResponse>> {
+        val result = MutableLiveData<Resource<TokenResponse>>()
+        val response = authApi.getUserToken(userToken)
+
+        when {
+            response.isSuccessful -> {
+                result.postValue(Resource.Success<TokenResponse>(data = response.body()!!))
+            }
+            response.code() == 400 -> {
+                result.postValue(Resource.Error<TokenResponse>(state = ErrorState.ERROR_400))
+            }
+            response.code() == 401 -> {
+                result.postValue(Resource.Error<TokenResponse>(state = ErrorState.ERROR_401))
+            }
+            response.code() == 422 -> {
+                result.postValue(Resource.Error<TokenResponse>(state = ErrorState.ERROR_422))
+            }
+        }
+
+        return result
+    }
+}
