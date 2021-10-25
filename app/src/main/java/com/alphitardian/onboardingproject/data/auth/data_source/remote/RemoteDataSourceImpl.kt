ea867ea772bd6ff1a55f0e4d6e1cef031e159d25
Ardian Alphita <ardianpramudya81@gmail.com>
2021-10-25 20:42:49 +0700

    Change implementation on auth data source

--- app/src/main/java/com/alphitardian/onboardingproject/data/auth/data_source/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/alphitardian/onboardingproject/data/auth/data_source/remote/RemoteDataSourceImpl.kt
@@ -1,6 +1,5 @@
 package com.alphitardian.onboardingproject.data.auth.data_source.remote
 
-import com.alphitardian.onboardingproject.common.Resource
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.network.AuthApi
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.LoginRequest
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.TokenResponse
@@ -9,21 +8,11 @@ import javax.inject.Singleton
 
 @Singleton
 class RemoteDataSourceImpl @Inject constructor(private val authApi: AuthApi) : RemoteDataSource {
-    override suspend fun loginUser(requestBody: LoginRequest): Resource<TokenResponse> {
-        return try {
-            val response = authApi.loginUser(requestBody)
-            Resource.Success<TokenResponse>(data = response)
-        } catch (error: Throwable) {
-            Resource.Error<TokenResponse>(error = error)
-        }
+    override suspend fun loginUser(requestBody: LoginRequest): TokenResponse {
+        return authApi.loginUser(requestBody)
     }
 
-    override suspend fun getToken(userToken: String): Resource<TokenResponse> {
-        return try {
-            val response = authApi.getUserToken(userToken)
-            Resource.Success<TokenResponse>(data = response)
-        } catch (error: Throwable) {
-            Resource.Error<TokenResponse>(error = error)
-        }
+    override suspend fun getToken(userToken: String): TokenResponse {
+        return authApi.getUserToken(userToken)
     }
 }
--- app/src/main/java/com/alphitardian/onboardingproject/domain/use_case/get_token/GetTokenUseCase.kt
+++ app/src/main/java/com/alphitardian/onboardingproject/domain/use_case/get_token/GetTokenUseCase.kt
@@ -1,12 +1,11 @@
 package com.alphitardian.onboardingproject.domain.use_case.get_token
 
-import com.alphitardian.onboardingproject.common.Resource
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.TokenResponse
 import com.alphitardian.onboardingproject.domain.repository.AuthRepository
 import javax.inject.Inject
 
 class GetTokenUseCase @Inject constructor(private val repository: AuthRepository) {
-    suspend operator fun invoke(userToken: String): Resource<TokenResponse> {
+    suspend operator fun invoke(userToken: String): TokenResponse {
         return repository.getUserToken(userToken)
     }
 }
