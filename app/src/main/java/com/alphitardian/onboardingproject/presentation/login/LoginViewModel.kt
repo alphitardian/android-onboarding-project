9bd3961d2aa3ffdb928d4de1625b581e22f3d571
Ardian Alphita <ardianpramudya81@gmail.com>
2021-10-25 09:19:49 +0700

    Add encryption implementation in login viewmodel

--- app/src/main/java/com/alphitardian/onboardingproject/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/alphitardian/onboardingproject/presentation/login/LoginViewModel.kt
@@ -1,20 +1,34 @@
 package com.alphitardian.onboardingproject.presentation.login
 
+import android.content.Context
+import android.os.Build
+import android.util.Base64
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.alphitardian.onboardingproject.common.KeystoreHelper
 import com.alphitardian.onboardingproject.common.Resource
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.LoginRequest
 import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.TokenResponse
+import com.alphitardian.onboardingproject.datastore.PrefStore
 import com.alphitardian.onboardingproject.domain.use_case.user_login.UserLoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val loginUseCase: UserLoginUseCase) : ViewModel() {
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: UserLoginUseCase,
+    @ApplicationContext context: Context,
+) : ViewModel() {
     var email = mutableStateOf("")
     var password = mutableStateOf("")
     var loading = mutableStateOf(false)
@@ -22,6 +36,9 @@ class LoginViewModel @Inject constructor(private val loginUseCase: UserLoginUseC
     var mutableLoginState: MutableLiveData<Resource<TokenResponse>> = MutableLiveData()
     val loginState: LiveData<Resource<TokenResponse>> get() = mutableLoginState
 
+    private val datastore = PrefStore(context)
+
+    @RequiresApi(Build.VERSION_CODES.O)
     fun loginUser() {
         viewModelScope.launch {
             loading.value = true
@@ -31,7 +48,49 @@ class LoginViewModel @Inject constructor(private val loginUseCase: UserLoginUseC
 
             mutableLoginState.postValue(result)
 
+            if (result is Resource.Success) {
+                val time = result.data.expiresTime
+                val localDate = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME)
+                val epoch = localDate.atZone(ZoneOffset.UTC).toInstant().toEpochMilli()/1000
+
+                saveExpiredTime(epoch)
+                encryptToken(result.data.token)
+            }
+
             loading.value = false
+        }
+    }
+
+    private fun saveToken(token: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            datastore.saveToken(token)
+        }
+    }
+
+    private fun saveTokenIV(iv : String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            datastore.saveTokenInitializationVector(iv)
+        }
+    }
+
+    private fun saveExpiredTime(time: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            datastore.saveExpiredTime(time)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun encryptToken(token: String) {
+        viewModelScope.launch {
+            KeystoreHelper.keyGenerator.init(KeystoreHelper.keyGenParameterSpec)
+            KeystoreHelper.keyGenerator.generateKey()
+
+            val encrypt = KeystoreHelper.encrypt(token.toByteArray())
+            val encryptedToken = Base64.encodeToString(encrypt["encrypted"], Base64.DEFAULT)
+            val iv = Base64.encodeToString(encrypt["iv"], Base64.DEFAULT)
+
+            saveToken(encryptedToken)
+            saveTokenIV(iv)
         }
     }
 }
