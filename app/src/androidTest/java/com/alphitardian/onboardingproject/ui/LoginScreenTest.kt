ba24c1ae1e060401e18d0fc22e92d2744df81d66
Ardian Alphita <ardianpramudya81@gmail.com>
2021-10-28 14:18:54 +0700

    Add idling resource dependency for UI test

---
+++ app/src/androidTest/java/com/alphitardian/onboardingproject/ui/LoginScreenTest.kt
@@ -0,0 +1,17 @@
+package com.alphitardian.onboardingproject.ui
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.alphitardian.onboardingproject.MainActivity
+import org.junit.Before
+import org.junit.Rule
+
+class LoginScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setup() {
+
+    }
+}
