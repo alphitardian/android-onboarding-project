2025d3d869fa78c1185b4d25a5fdf56d29a50612
Ardian Alphita <ardianpramudya81@gmail.com>
2021-10-29 14:57:56 +0700

    Add viewmodel test in unit test

---
+++ app/src/test/java/com/alphitardian/onboardingproject/presentation/home/HomeViewModelTest.kt
@@ -0,0 +1,137 @@
+package com.alphitardian.onboardingproject.presentation.home
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
+import com.alphitardian.onboardingproject.common.Resource
+import com.alphitardian.onboardingproject.data.user.data_source.local.entity.NewsEntity
+import com.alphitardian.onboardingproject.data.user.data_source.local.entity.UserEntity
+import com.alphitardian.onboardingproject.data.user.data_source.remote.response.news.toNewsEntity
+import com.alphitardian.onboardingproject.data.user.data_source.remote.response.user.toUserEntity
+import com.alphitardian.onboardingproject.data.user.repository.UserRepositoryImpl
+import com.alphitardian.onboardingproject.domain.repository.UserRepository
+import com.alphitardian.onboardingproject.domain.use_case.decrypt_token.DecryptTokenUseCase
+import com.alphitardian.onboardingproject.domain.use_case.encrypt_token.EncryptTokenUseCase
+import com.alphitardian.onboardingproject.domain.use_case.get_news.GetNewsUseCase
+import com.alphitardian.onboardingproject.domain.use_case.get_profile.GetProfileUseCase
+import com.alphitardian.onboardingproject.domain.use_case.get_token.GetTokenUseCase
+import com.alphitardian.onboardingproject.utils.DummyData
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class HomeViewModelTest {
+
+    @get:Rule
+    var mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private var context = ApplicationProvider.getApplicationContext<Context>()
+
+    private lateinit var viewModel: HomeViewModel
+
+    @Mock
+    private lateinit var userRepository: UserRepository
+
+    @Mock
+    private lateinit var profileUseCase: GetProfileUseCase
+
+    @Mock
+    private lateinit var newsUseCase: GetNewsUseCase
+
+    @Mock
+    private lateinit var tokenUseCase: GetTokenUseCase
+
+    @Mock
+    private lateinit var encryptTokenUseCase: EncryptTokenUseCase
+
+    @Mock
+    private lateinit var decryptTokenUseCase: DecryptTokenUseCase
+
+    @Mock
+    private lateinit var profileObserver: Observer<Resource<UserEntity>>
+
+    @Mock
+    private lateinit var newsObserver: Observer<Resource<List<NewsEntity>>>
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+
+        userRepository = Mockito.mock(UserRepositoryImpl::class.java)
+        profileUseCase = Mockito.mock(GetProfileUseCase::class.java)
+        newsUseCase = Mockito.mock(GetNewsUseCase::class.java)
+        tokenUseCase = Mockito.mock(GetTokenUseCase::class.java)
+        encryptTokenUseCase = Mockito.mock(EncryptTokenUseCase::class.java)
+        decryptTokenUseCase = Mockito.mock(DecryptTokenUseCase::class.java)
+
+        viewModel = HomeViewModel(
+            context = context,
+            tokenUseCase = tokenUseCase,
+            newsUseCase = newsUseCase,
+            profileUseCase = profileUseCase,
+            encryptTokenUseCase = encryptTokenUseCase,
+            decryptTokenUseCase = decryptTokenUseCase
+        )
+    }
+
+    @Test
+    fun testGetProfile() {
+        runBlocking {
+            val dummyResponse = MutableLiveData<Resource<UserEntity>>()
+            dummyResponse.value =
+                Resource.Success<UserEntity>(data = DummyData.expectedProfileResponse.toUserEntity())
+
+            Mockito.`when`(userRepository.getUserProfile(DummyData.userToken))
+                .thenReturn(DummyData.expectedProfileResponse.toUserEntity())
+            Mockito.`when`(profileUseCase.invoke(DummyData.userToken))
+                .thenReturn(DummyData.expectedProfileResponse.toUserEntity())
+
+            viewModel.getUserProfile(DummyData.userToken)
+
+            viewModel.profile.observeForever(profileObserver)
+
+            delay(1000)
+
+            Mockito.verify(profileObserver).onChanged(Resource.Loading())
+            Mockito.verify(profileObserver).onChanged(dummyResponse.value)
+        }
+    }
+
+    @Test
+    fun testGetNews() {
+        runBlocking {
+            val dummyResponse = MutableLiveData<Resource<List<NewsEntity>>>()
+            val dummyData = DummyData.expectedNewsResponse.data.map { it.toNewsEntity() }
+            dummyResponse.value = Resource.Success<List<NewsEntity>>(data = dummyData)
+
+            Mockito.`when`(userRepository.getNews(DummyData.userToken)).thenReturn(dummyData)
+            Mockito.`when`(newsUseCase.invoke(DummyData.userToken)).thenReturn(dummyData)
+
+            viewModel.getUserNews(DummyData.userToken)
+
+            viewModel.news.observeForever(newsObserver)
+
+            delay(1000)
+
+            Mockito.verify(newsObserver).onChanged(Resource.Loading())
+            Mockito.verify(newsObserver).onChanged(dummyResponse.value)
+        }
+    }
+}
---
+++ app/src/test/java/com/alphitardian/onboardingproject/presentation/login/LoginViewModelTest.kt
@@ -0,0 +1,102 @@
+package com.alphitardian.onboardingproject.presentation.login
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
+import com.alphitardian.onboardingproject.common.Resource
+import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.LoginRequest
+import com.alphitardian.onboardingproject.data.auth.data_source.remote.response.TokenResponse
+import com.alphitardian.onboardingproject.data.auth.repository.AuthRepositoryImpl
+import com.alphitardian.onboardingproject.domain.repository.AuthRepository
+import com.alphitardian.onboardingproject.domain.use_case.encrypt_token.EncryptTokenUseCase
+import com.alphitardian.onboardingproject.domain.use_case.user_login.UserLoginUseCase
+import com.alphitardian.onboardingproject.utils.DummyData
+import com.alphitardian.onboardingproject.utils.FakeAndroidKeyStore
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class LoginViewModelTest {
+
+    companion object {
+        @JvmStatic
+        @BeforeClass
+        fun beforeClass() {
+            FakeAndroidKeyStore.setup
+        }
+    }
+
+    @get:Rule
+    var mockitoRule: MockitoRule = MockitoJUnit.rule()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private var context = ApplicationProvider.getApplicationContext<Context>()
+
+    private lateinit var viewModel: LoginViewModel
+
+    @Mock
+    private lateinit var authRepository: AuthRepository
+
+    @Mock
+    private lateinit var loginUseCase: UserLoginUseCase
+
+    @Mock
+    private lateinit var encryptTokenUseCase: EncryptTokenUseCase
+
+    @Mock
+    private lateinit var observer: Observer<Resource<TokenResponse>>
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+
+        authRepository = Mockito.mock(AuthRepositoryImpl::class.java)
+        loginUseCase = Mockito.mock(UserLoginUseCase::class.java)
+        encryptTokenUseCase = Mockito.mock(EncryptTokenUseCase::class.java)
+        viewModel = LoginViewModel(
+            loginUseCase = loginUseCase,
+            encryptTokenUseCase = encryptTokenUseCase,
+            context = context
+        )
+    }
+
+    @Test
+    fun loginUser() {
+        runBlocking {
+            val dummyResponse = MutableLiveData<Resource<TokenResponse>>()
+            dummyResponse.value =
+                Resource.Success<TokenResponse>(data = DummyData.expectedTokenResponse)
+
+            viewModel.email.value = "tester"
+            viewModel.password.value = "tester123"
+
+            Mockito.`when`(authRepository.loginUser(LoginRequest("tester123", "tester")))
+                .thenReturn(
+                    DummyData.expectedTokenResponse)
+            Mockito.`when`(loginUseCase.invoke(LoginRequest("tester123",
+                "tester"))).thenReturn(DummyData.expectedTokenResponse)
+
+            viewModel.loginUser()
+
+            viewModel.loginState.observeForever(observer)
+
+            Mockito.verify(observer).onChanged(dummyResponse.value)
+        }
+    }
+}
